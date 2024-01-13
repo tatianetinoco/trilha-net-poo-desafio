49e0acab2109c56ab2e40ff025078160c0757de7
tatianetinoco <tatianeperr@gmail.com>
2024-01-13 16:53:48 -0300

    Desafio POO em C# Concluído - Trilha .NET

--- Program.cs
+++ Program.cs
@@ -1,3 +1,15 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Console.WriteLine("Smartphone Nokia:");
+Smartphone nokia = new Nokia(numero: "123456", modelo: "Modelo 1", imei: "11111111", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Whatsapp");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone IPhone:");
+Smartphone iPhone = new Iphone(numero: "7891011", modelo: "Modelo 2", imei: "22222222", memoria: 128);
+iPhone.Ligar();
+iPhone.InstalarAplicativo("Telegram");
